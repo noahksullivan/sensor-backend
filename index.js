97e3b57edd46f05c6916519ec2a9696f88dae74f
Noah Sullivan <noahsullivan25@gmail.com>
2026-06-12 22:15:25 -0400

    Add CORS for public web frontend

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
 
 app.use(express.json());
 
